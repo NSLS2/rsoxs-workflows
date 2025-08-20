8f2a45379484aff0dac5163bba9995c5a900cc03
Jun Aishima <jaishima@bnl.gov>
2025-08-20 14:51:55 -0400

    fix name of deployment in run-deployment script

--- run-deployment.py
+++ run-deployment.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from prefect.deployments import run_deployment
 
 run_deployment(
-    name="end-of-run-workflow/rsoxs-workflow-prefect3-deploy",
+    name="end-of-run-workflow/rsoxs-end-of-run-workflow-docker",
     parameters={"stop_doc": {"run_start": ""}},
     timeout=15,  # don't wait for the run to finish # edit to 15 sec
 )
