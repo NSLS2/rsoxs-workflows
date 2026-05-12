f803411f7fd4cd6fbf846e8f2b27911cc2d46247
Jun Aishima <jaishima@bnl.gov>
2026-05-12 08:59:58 -0400

    pre-commit fixes

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,16 +1,78 @@
-import os
+import traceback
+
 from prefect import flow, get_run_logger, task
+from prefect.blocks.notifications import SlackWebhook
+from prefect.context import FlowRunContext
+from prefect.settings import PREFECT_UI_URL
 
 # from data_validation import general_data_validation
 from export import export
-from dotenv import load_dotenv
+from data_validation import get_run, get_api_key_from_env
+
+CATALOG_NAME = "rsoxs"
 
 
-def get_api_key_from_env():
-    with open("/srv/container.secret", "r") as secrets:
-        load_dotenv(stream=secrets)
-    api_key = os.environ["TILED_API_KEY"]
-    return api_key
+def slack(func):
+    """
+    Send a message to mon-prefect and mon-prefect-spec slack channels if the flow-run failed.
+    Send a message to mon-prefect-rsoxs slack channel with the flow-run status.
+    Send a message to mon-bluesky slack channel if the bluesky-run failed.
+
+    NOTE: the name of this inner function is the same as the real end_of_workflow() function because
+    when the decorator is used, Prefect sees the name of this inner function as the name of
+    the flow. To keep the naming of workflows consistent, the name of this inner function had to match the expected name.
+    """
+
+    def end_of_run_workflow(stop_doc, api_key=None, dry_run=False):
+        flow_run_name = FlowRunContext.get().flow_run.dict().get("name")
+
+        # Load slack credentials that are saved in Prefect.
+        mon_prefect = SlackWebhook.load("mon-prefect")
+        mon_bluesky = SlackWebhook.load("mon-bluesky")
+        mon_prefect_rsoxs = SlackWebhook.load("mon-prefect-rsoxs")
+        mon_prefect_spec = SlackWebhook.load("mon-prefect-spec")
+
+        # Get the uid.
+        uid = stop_doc["run_start"]
+
+        # Get Tiled API key, if not set already
+        if not api_key:
+            api_key = get_api_key_from_env()
+
+        # Get the scan_id.
+        run = get_run(uid, api_key=api_key)
+        scan_id = run.start["scan_id"]
+
+        # Send a message to mon-bluesky if bluesky-run failed.
+        if stop_doc.get("exit_status") == "fail":
+            mon_bluesky.notify(
+                f":bangbang: {CATALOG_NAME} bluesky-run failed. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```reason: {stop_doc.get('reason', 'none')}```"
+            )
+
+        try:
+            result = func(stop_doc, api_key=api_key, dry_run=dry_run)
+
+            # Send a message to mon-prefect-rsoxs if flow-run is successful.
+            message = f":white_check_mark: {CATALOG_NAME} flow-run successful. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}```"
+            mon_prefect_rsoxs.notify(message)
+            return result
+        except Exception as e:
+            tb = traceback.format_exception_only(e)
+
+            # Send a message to mon-prefect-rsoxs, mon-prefect if flow-run failed.
+            message = f":bangbang: {CATALOG_NAME} flow-run failed. (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```{tb[-1]}```"
+            mon_prefect.notify(message)
+            mon_prefect_rsoxs.notify(message)
+            flow_run = FlowRunContext.get().flow_run
+            # Add link to flow-run for the message to mon-prefect-spec.
+            program_message = (
+                f":bangbang: {CATALOG_NAME} flow-run failed. <https://{PREFECT_UI_URL.value()}/flow-runs/"
+                + f"flow-run/{flow_run.id}|the flow run link> (*{flow_run_name}*)\n ```run_start: {uid}\nscan_id: {scan_id}``` ```{tb[-1]}```"
+            )
+            mon_prefect_spec.notify(program_message)
+            raise
+
+    return end_of_run_workflow
 
 
 @task
@@ -21,6 +83,7 @@ def log_completion():
 
 @flow
 def end_of_run_workflow(stop_doc, api_key=None, dry_run=None):
+    print(f"Initial value: dry_run={dry_run}")
     uid = stop_doc["run_start"]
     if not api_key:
         api_key = get_api_key_from_env()
